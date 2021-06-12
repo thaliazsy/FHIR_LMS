5705bd99838a3900867fea4d9d60b2cd57e700c9
謝愛佳 <victoriatjiaa@gmail.com>
2021-06-13 00:25:00 +0800

    Update course selected function
    Next task:
    - Divide Slot between 1 ... n 堂課

--- course1_正念課程/js/setting.js
+++ course1_正念課程/js/setting.js
@@ -1,23 +1,25 @@
 //Declare variable
-let FHIRserver= 'http://hapi.fhir.org/baseR4/';
+let FHIRserver= 'http://203.64.84.213:8080/fhir/';
 //'http://hapi.fhir.org/baseR4/';
 //'http://203.64.84.213:8080/fhir/';
-let globalPatientID, globalName, globalPersonID;
+let globalPatientID, globalName, globalPersonID, globalIndex0, globalIndex1;
 let index=0, iOrganization=-1, iAppointment=0, iSlot=0, iSchedule=0,  i3=0, i4=0;
 let iTeacher=0;
 let curUserID, curOrganizationID, curOrganizationName;
 let tableValue, tableValue2, arrTempSlot, arrSchedule;	
-let arrTeacherID= new Array(), arrTeacherName, arrSchedule2;					
-let arrOrganization2ID= ["2137917", "2139336"];
-let arrOrganization2Name= ["testOrganization1", "testOrganization2"];
-let iSchedule2= [0, 0];
+let arrTeacherID= new Array(), arrTeacherName, arrSchedule2, arrScheduleID;					
+let arrOrganization2ID= ["2220"];		//, "1735", "1762"
+let arrOrganization2Name= ["testOrganization1"];	//, "慈濟大學", "陽明大學"
+let iSchedule2= [0, 0];		//organization n Total Schedule
 tableValue=createMultiArray(tableValue, 4, 5, 5, 10, 30);		//total Organization (patient or practitioner) //total Schedule （Course taken） //0.userID(Patient or Practitioner), 1.organizationName, 2.slotID, 3.Slot startTime, 4.Slot endTime, 5.scheduleID, 6.videoName, 7.courseName, 8.videoURL, 9.questionnaireURL, 10.practitionerRoleID, 11.Course start date, 12.Course end date, 13.practitionerName
-tableValue2= createMultiArray(tableValue2, 3, 5, 5, 30, 0);
+tableValue2= createMultiArray(tableValue2, 3, 5, 5, 36, 0);
 arrTempSlot= createMultiArray(arrTempSlot, 2, 10, 4, 0, 0);	//temporary store all slot of this member	//slot id, start date, end date, schedule id
 arrSchedule= createMultiArray(arrSchedule, 2, 5, 6, 0, 0);		//store Schedule id	//course name, teacher name, start date, end date, questionnaire1 URL, total slot
-arrTeacherName= createMultiArray(arrSchedule, 2, 5, 2, 0, 0);
-arrSchedule2= createMultiArray(arrSchedule, 2, 5, 2, 0, 0);
-let arrOrganization= new Array(5);							//store organization name
+arrTeacherName= createMultiArray(arrTeacherName, 2, 5, 2, 0, 0);
+arrSchedule2= createMultiArray(arrSchedule2, 2, 5, 2, 0, 0);
+arrScheduleID= createMultiArray(arrSchedule, 2, 5, 6, 0, 0);							//store Schedule id with organization index
+let arrOrganization= new Array();							//store organization name
+let arrOrganizationID= new Array();							//store organization ID
 let arrTempSchedule= new Array();							//store Schedule id
 let personJSONobj, patientJSONobj, appointmentJSONobj;
 //END: Declare variable
@@ -30,7 +32,7 @@ function initialize(){
 				"value": "test1"
 			}, {
 				"system": "Password",
-				"value": "MWI0ZjBlOTg1MTk3MTk5OGU3MzIwNzg1NDRjOTZiMzZjM2QwMWNlZGY3Y2FhMzMyMzU5ZDZmMWQ4MzU2NzAxNA=="
+				"value": ""
 			} ],
 			"name": [ {
 				"text": "testPerson1"
@@ -41,12 +43,32 @@ function initialize(){
 				  "value": "Jim@example.org"
 				}
 			]
-			//"link": [ {
-				//"target": {
-					//"reference": "Patient/2138343",
-					//"display": "testPatient1"
-				//}
-			//} ]
+        };
+		
+	personJSONobj2 = {
+            "resourceType": "Person",
+            "identifier": [ {
+				"system": "UserID",
+				"value": "test1"
+			}, {
+				"system": "Password",
+				"value": ""
+			} ],
+			"name": [ {
+				"text": "testPerson1"
+			} ],
+			"telecom": [
+				{
+				  "system": "email",
+				  "value": "Jim@example.org"
+				}
+			],
+			"link": [ {
+				"target": {
+					"reference": "Patient/",
+					"display": "testPatient1"
+				}
+			} ]
         };
 	
 	patientJSONobj = {
@@ -94,12 +116,16 @@ function checkRequiredField(reqFieldNum){
 			var emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 			if (Total_Obj[k].value != ""){
 				if(emailFormat.test(Total_Obj[k].value)) count++;
-				else alert("Email invalid");
+				else{ 
+					alert("Email 格式錯誤");
+					document.getElementById("btnSubmit").disabled = false;
+				}
 			}
 		}
 	}
 	if (reqFieldNum != count){
 		alert("表單未填寫完畢");
+		document.getElementById("btnSubmit").disabled = false;
 		return 0;
 	}
 	return 1;
@@ -136,6 +162,7 @@ function getFHIRMyCourse(jsonOBJ, type){	//我的課程
 		iOrganization++;
 		curUserID= jsonOBJ.id;
 		curOrganizationID = jsonOBJ.managingOrganization.reference;
+		arrOrganizationID.push(curOrganizationID);
 		let urlStr= FHIRserver + curOrganizationID;
 		HTTPGetData(urlStr, "Organization");
 	}
@@ -145,7 +172,7 @@ function getFHIRMyCourse(jsonOBJ, type){	//我的課程
 		HTTPGetData(urlStr, "Appointment");	
 	}
 	else if (type=="Appointment"){
-		if (jsonOBJ.total == 0)	alert('data unexist');
+		if (jsonOBJ.total == 0)	alert('No appointment');
 		else{
 			for (var i=0;i<((jsonOBJ.total>10)?10:jsonOBJ.total);i++){ 
 				arrTempSlot[iAppointment] = jsonOBJ.entry[i].resource.slot[0].reference;			//slotID
@@ -189,7 +216,7 @@ function getFHIRMyCourse(jsonOBJ, type){	//我的課程
 	else if (type=="Schedule"){
 		var index = arrTempSchedule.indexOf("Schedule/" + jsonOBJ.id);								
 		var txt= jsonOBJ.specialty[0].coding[0].display.split('-');
-		arrSchedule[index][0] = txt[1];					//0.courseName
+		arrSchedule[index][0] = txt[1];													//0.courseName
 		arrSchedule[index][1] = jsonOBJ.actor[0].reference;								//1.practitionerRoleID
 		txt= jsonOBJ.planningHorizon.start.split("T");
 		arrSchedule[index][2] = txt[0];													//2.Course start date
@@ -233,17 +260,19 @@ function getFHIRCourseList(jsonOBJ, type){	//課程清單
 		else if (type=="Schedule2"){
 			let locIndex=0;
 			for (var i=0;i<((jsonOBJ.total>10)?10:jsonOBJ.total);i++){ 
-				let index = arrTeacherID.indexOf(jsonOBJ.entry[i].resource.actor[0].reference);										//practitionerRoleID
+				let index = arrTeacherID.indexOf(jsonOBJ.entry[i].resource.actor[0].reference);									//practitionerRoleID
 				let indexOrganization = arrOrganization2Name.indexOf(arrTeacherName[index][1]);
 				let iTemp= iSchedule2[indexOrganization];
-				arrSchedule2[iTemp][0]= jsonOBJ.entry[i].resource.id;
+				arrSchedule2[iTemp][0]= jsonOBJ.entry[i].resource.id;			//scheduleID
 				arrSchedule2[iTemp][1]= indexOrganization;
-				tableValue2[indexOrganization][iTemp][0] = jsonOBJ.entry[i].resource.specialty[0].coding[0].display;			//course name			
-				tableValue2[indexOrganization][iTemp][1] = arrTeacherName[index][0];											//teacher name									
+				arrScheduleID[indexOrganization][iTemp]= "Schedule/" + jsonOBJ.entry[i].resource.id;
+				tableValue2[indexOrganization][iTemp][0] = arrTeacherID[index];													//practitionerRoleID
+				tableValue2[indexOrganization][iTemp][1] = jsonOBJ.entry[i].resource.specialty[0].coding[0].display;			//course name			
+				tableValue2[indexOrganization][iTemp][2] = arrTeacherName[index][0];											//teacher name									
 				var txt= jsonOBJ.entry[i].resource.planningHorizon.start.split("T");
-				tableValue2[indexOrganization][iTemp][2] = txt[0];
-				txt= jsonOBJ.entry[i].resource.planningHorizon.end.split("T");
 				tableValue2[indexOrganization][iTemp][3] = txt[0];
+				txt= jsonOBJ.entry[i].resource.planningHorizon.end.split("T");
+				tableValue2[indexOrganization][iTemp][4] = txt[0];
 				iSchedule2[indexOrganization]++;
 				locIndex++;
 			}
@@ -253,30 +282,75 @@ function getFHIRCourseList(jsonOBJ, type){	//課程清單
 			}
 		}
 		else if (type=="Slot2"){
+			iSlot2=0;
 			for (var i=0;i<((jsonOBJ.total>10)?10:jsonOBJ.total);i++){ 
 				if(jsonOBJ.entry[i].resource.status=="free"){
-					tableValue2[arrSchedule2[ScheduleIndex][1]][ScheduleIndex][iSlot2+4] = jsonOBJ.entry[i].resource.id;						//practitionerRoleID
+					tableValue2[arrSchedule2[ScheduleIndex][1]][ScheduleIndex][iSlot2+5] = "Slot/" + jsonOBJ.entry[i].resource.id;						//practitionerRoleID
 					iSlot2++;
 				}
 			}
 			ScheduleIndex++;
 			
-			if(iSchedule2.length==ScheduleIndex) createTable2();
+			if(iSchedule2[0]==ScheduleIndex) createTable2();
 		}
 	}
 }
 
-var arrPatient= new Array(5);
-function getFHIRSelectCourse(retValue, type){	//選課
-	var jsonOBJ =JSON.parse(retValue);
-	if (type=="Person2"){
-		for(var i=0; i<jsonOBJ.link.length; i++){
-			arrPatient[i]= jsonOBJ.link.target.reference;
+function checkOrganization(field){
+	var existOrganization=0;
+	field= field.value.split('&');
+	globalIndex0= parseInt(field[0]);
+	globalIndex1= parseInt(field[1]);
+	var temp= "Organization/" + arrOrganization2ID[globalIndex0];
+	arrOrganizationID.forEach(function (item, index){
+		if( temp == item){	//field.value= organization
+			existOrganization=1;
+			return;
 		}
-		
-		for(var i=0; i<arrPatient.length; i++){
-			let urlStr= FHIRserver + arrPatient[i];
-			HTTPGetData(urlStr, "Patient2");
+	});
+	
+	//organizationID, scheduleID, practitionerRoleID, practitionerName
+	//var param= 'Organization/' + arrOrganization2ID[index0] + '&' + arrScheduleID[index0][index1] + '&' + tableValue2[index0][index1][0] + '&' + tableValue2[index0][index1][1];					
+	
+	if(existOrganization==0)	createPatient(temp);
+	else{
+		let urlStr= FHIRserver + "Slot?schedule=reference|" + arrScheduleID[globalIndex0][globalIndex1];
+		HTTPGetData(urlStr, "getSlotOfSchedule");
+	}
+}
+
+function createPatient(organizationID){
+	initialize();
+	patientJSONobj.name[0].text= globalName;
+	patientJSONobj.managingOrganization.reference= organizationID;
+	patientJSONobj = JSON.stringify(patientJSONobj);
+	HTTPPostData(FHIRserver + "Patient", patientJSONobj, "postPatient");
+}
+
+var arrPatient= new Array(5);
+function getFHIRSelectCourse(jsonOBJ, type){	//選課
+	if (type=="getSlotOfSchedule"){
+		slotExist=0;
+		if (jsonOBJ.total == 0)	var iiii=0;
+		else{
+			for (var i=0;i<((jsonOBJ.total>10)?10:jsonOBJ.total);i++){ 
+				if(jsonOBJ.entry[i].resource.status=="free"){
+					initialize();
+					appointmentJSONobj.slot[0].reference= "Slot/" + jsonOBJ.entry[i].resource.id;							//slot ID
+					appointmentJSONobj.participant[0].actor.reference= globalPatientID;										//patient ID
+					appointmentJSONobj.participant[0].actor.display= globalName;											//patient name
+					appointmentJSONobj.participant[1].actor.reference= tableValue2[globalIndex0][globalIndex1][0];			//PractitionerRole ID
+					appointmentJSONobj.participant[1].actor.display= tableValue2[globalIndex0][globalIndex1][1];			//PractitionerRole name
+					appointmentJSONobj = JSON.stringify(appointmentJSONobj);
+					HTTPPostData(FHIRserver + "Appointment", appointmentJSONobj, jsonOBJ.entry[i].resource);
+					slotExist=1;
+					break;
+				}
+			}
+			if(slotExist==0){
+				alert("報名人數已額滿！");
+				document.getElementById("btnSubmit").disabled = false;
+			}
 		}
 	}
 	// else if (type=="Patient2"){
@@ -285,80 +359,6 @@ function getFHIRSelectCourse(retValue, type){	//選課
 		// // else{
 			// // //create patient
 		// // }
-	// }
-}
-
-function createTable(){
-	var table= document.getElementById("TableAppointment");
-	var cellIndex;
-	var row, noIndex=1, videoName;
-	for (var index0=0;index0<=iOrganization;index0++){			//check total organization
-		table.innerHTML+= '<tr><th bgcolor="#ebebe0" colspan="2">Organization Name: ' + arrOrganization[index0] + '<br>My Course List</th></tr>';
-		for (var index1=0;index1<iSchedule;index1++){			//divide per schedule (per mata pelajaran)
-			table.innerHTML+= '<tr><td colspan="3">課程名稱: ' + arrSchedule[index1][0] + '<br>教授: ' + arrSchedule[index1][1] + '<br>課程期間: ' + arrSchedule[index1][2] + ' 至 ' + arrSchedule[index1][3] + '<br><a href="' + arrSchedule[index1][4] + '" target="_blank" style="color: blue;">問卷 URL</a></td></tr>';
-			table.innerHTML+= '<tr><th>No.</th><th>Video</th></tr>';
-			var totSlot= arrSchedule[index1][5];
-			for (var index2=0;index2<totSlot;index2+=5){		//per Slot
-				var totVideo=tableValue[index0][index1][index2][2];
-				for (var index3=3;index3<totVideo*2;index3+=2){	//total video
-					row = table.insertRow(-1);
-					videoName=tableValue[index0][index1][index2][index3].split("_");
-					row.insertCell(0).innerHTML= videoName[0];
-					var elLink = document.createElement('a');
-					elLink.href = tableValue[index0][index1][index2];
-					elLink.innerHTML = videoName[1];
-					row.insertCell(1).appendChild(elLink);
-				}
-				row = table.insertRow(-1);
-				elLink = document.createElement('a');
-				elLink.target= "_blank";
-				elLink.href = tableValue[index0][index1][index2][totVideo*2+3];
-				elLink.innerHTML = "問卷 URL";
-				var x = row.insertCell(0);
-				x.appendChild(elLink);
-				x.colSpan=2;
-				x.align="center";
-			}
-		}
-	}
-}
-
-function createTable2(){
-	var table= document.getElementById("TableCourseSelection");
-	var cellIndex;
-	var row, noIndex=1, txt;
-	for (var index0=0;index0<=iOrganization2;index0++){		//check total organization
-		table.innerHTML+= '<tr><th bgcolor="#ebebe0" colspan="5">Organization Name: ' + arrOrganization2Name[index0] + '</th></tr>';
-		table.innerHTML+= '<tr><th>No.</th><th>Course Name</th><th>Teacher</th><th>Period</th><th>Select</th></tr>';
-		noIndex=1;
-		for (var index1=0;index1<iSchedule2[index0];index1++){		//divide per schedule (per mata pelajaran)
-			txt= '<tr><td>' + noIndex + '</td><td>' + tableValue2[index0][index1][0] + '</td><td>' + tableValue2[index0][index1][1] + '</td><td>' + tableValue2[index0][index1][2] + ' 至 ' + tableValue2[index0][index1][3] + '</td>';
-			
-			if (tableValue2[index0][index1][4] == null)
-				txt+= '<td style="color: red">額滿</td></tr>'
-				//document.getElementById("myBtn").disabled = false;
-			else	
-				txt+= '<td><button value="' + index0 + '&' + index1 + '" onclick="selectCourse(this.value)">選課</button></td></tr>'
-			
-			table.innerHTML+= txt;
-			noIndex++;
-		}
-	}
-}
-
-function selectCourse(val){
-	let index= val.split('&');
-	let index0= index[0];
-	let index1= index[1];
-	let i=4;
-	//cek patient apakah ad lian ke organization ini
-	let urlStr= FHIRserver + "Person/" + globalPersonID;
-	HTTPGetData(urlStr, "Person3");
-	// while(tableValue2[index0][index1][i] != null){
-		// tableValue2[index0][index1][4]
-		// 
-		
-		// i++;
 	// }
 }
 
@@ -372,7 +372,7 @@ function queryParam(){
 			if(ary[i].split('=')[0] == 'patientID')
 				globalPatientID = ary[i].split('=')[1];
 			else if(ary[i].split('=')[0] == 'name')
-				globalName = ary[i].split('=')[1];
+				globalName = decodeURIComponent(ary[i].split('=')[1]);
 			else if(ary[i].split('=')[0] == 'personID')
 				globalPersonID = ary[i].split('=')[1];
 		}
@@ -387,6 +387,22 @@ function HTTPGetData(urlStr, type) {
 			var jsonOBJ =JSON.parse(ret);
 			if(type == "loginVerify")	checkUsername(jsonOBJ);
 			else if(type == "CheckPersonUserID")	checkUsername(jsonOBJ);
+			else if(type == "getSlotOfSchedule")	getFHIRSelectCourse(jsonOBJ, type);
+			else if(type == "getPerson"){
+				// personJSONobj2.identifier[0].value= userID;
+				// personJSONobj2.identifier[1].value= encPassword;
+				// personJSONobj2.name[0].text= globalName;
+				// personJSONobj2.telecom[0].value= userID;
+				// personJSONobj2.link[0].target.reference= globalPatientID;
+				// personJSONobj2.link[0].target.display= globalName;
+				// personJSONobj2 = JSON.stringify(personJSONobj2);
+				//HTTPPostData(FHIRserver + "/Person", personJSONobj, "postPerson");
+				ret = ret.slice(0, -1);
+				let str= ',"link":[{"target":{"reference": "' + globalPatientID + '","display": "' + globalName + '"}}]}';
+				ret += str;
+				let urlStr= FHIRserver + 'Person/' + globalPersonID;
+				HTTPPutData(urlStr, ret, "putPerson");
+			}
 			else if(type=="PractitionerRole2" || type=="Schedule2" || type=="Slot2") getFHIRCourseList(jsonOBJ, type);
 			else getFHIRMyCourse(jsonOBJ, type);
 		}	
@@ -395,7 +411,7 @@ function HTTPGetData(urlStr, type) {
 	HttpObj.send();
 }
 
-function HTTPPostData(urlStr, dataStr) {
+function HTTPPostData(urlStr, dataStr, type) {
     var HttpObj = new XMLHttpRequest();
     HttpObj.open("POST", urlStr, true);
     HttpObj.setRequestHeader("Content-type", "application/json+fhir");
@@ -404,13 +420,35 @@ function HTTPPostData(urlStr, dataStr) {
         if (this.readyState === 4) {
             ret = this.responseText;
             //alert(ret);
+			
+			var jsonOBJ =JSON.parse(ret);
+			//check if error occured
+			if(jsonOBJ.resourceType == "OperationOutcome"){
+				alert('系統錯誤! 請聯絡慈大醫資龍昱璇學姊，\n電話：0965006102\n郵件：108316107@gms.tcu.edu.tw')
+				document.getElementById("btnSubmit").disabled = false;
+				return 0;
+			}
+			
+			if(type == "postPatient"){	
+				globalPatientID= "Patient/" + jsonOBJ.id;	
+				//get person
+				let urlStr= FHIRserver + "Person/" + globalPersonID;
+				HTTPGetData(urlStr, "getPerson");
+			}
+			else{
+				let slotID= type.id;
+				type.status="busy";
+				let urlStr= FHIRserver + 'Slot/' + slotID;
+				type = JSON.stringify(type);
+				HTTPPutData(urlStr, type, "PutSlot");
+			}
         }
     }
     HttpObj.send(dataStr);
 }
 
-function HTTPPutData(urlStr, dataStr) {
-    document.getElementById("TextArea1").value=dataStr;
+function HTTPPutData(urlStr, dataStr, type) {
+    //document.getElementById("TextArea1").value=dataStr;
 	var HttpObj = new XMLHttpRequest();
 	HttpObj.open("PUT", urlStr, true);
 	HttpObj.setRequestHeader("Content-type", "application/json+fhir");
@@ -418,6 +456,14 @@ function HTTPPutData(urlStr, dataStr) {
         if (HttpObj.readyState === 4) {
             ret = HttpObj.responseText;
 			//alert(ret);
+			if(type=="putPerson"){
+				let urlStr= FHIRserver + "Slot?schedule=reference|" + globalScheduleID;
+				HTTPGetData(urlStr, "getSlotOfSchedule");
+			}
+			else if("PutSlot"){
+				alert("選課完成！需等1-2分鐘才可以看到課程列表！");
+				location.reload();
+			}
         }
     }
     HttpObj.send(dataStr);
