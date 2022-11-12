2edd2622b37b6a770a7ed06d37c7f5f95d11ac2e
Victoria Tjia <36994634+victoriatjia@users.noreply.github.com>
2022-11-12 16:54:08 +0700

    Update fhir url

--- LIVE/misat-seminar/lms-includes/js/setting.js
+++ LIVE/misat-seminar/lms-includes/js/setting.js
@@ -1,5 +1,5 @@
 //Set FHIR Server URL and response type (json or xml)
-let FHIRURL= 'https://tzfhir.ml:53443/open/fhir/'; //default FHIR Server API 
+let FHIRURL= 'https://tzfhir.ml:53443/fhir/'; //default FHIR Server API 
 let FHIRResponseType= 'json';					//Requested data type returned by the server
 
 //Define required FHIR resources
