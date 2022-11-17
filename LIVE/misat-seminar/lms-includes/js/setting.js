bfa9cff5e42684e52c12e776bda4c7e65da316cb
jeshika <forimportjesh@gmail.com>
2022-11-17 10:50:36 +0800

    change select course api port to 443

--- LIVE/misat-seminar/lms-includes/js/setting.js
+++ LIVE/misat-seminar/lms-includes/js/setting.js
@@ -1,7 +1,7 @@
 //Set FHIR Server URL and response type (json or xml)
 let FHIRURLLogin= 'https://tzfhir.ml:53443/r4/rest/login';
 let FHIRURL= 'https://tzfhir.ml:53443/fhir/'; //default FHIR Server API 
-let SelectCourseAPI= "http://203.64.84.208:81/api/LSMAPI/SelectCourse"; //Select course API 
+let SelectCourseAPI= "http://203.64.84.208:443/api/LSMAPI/SelectCourse"; //Select course API 
 let FHIRResponseType= 'json';					//Requested data type returned by the server
 
 //Define required FHIR resources
