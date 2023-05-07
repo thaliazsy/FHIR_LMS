848becfcec57e5ea996f996d566cecca19fc6219
Thalia <thalia_noga@yahoo.com>
2023-05-07 21:47:55 +0800

    Add document details page

--- LIVE/portal/assets/js/AuthenticationService/view-document.js
+++ LIVE/portal/assets/js/AuthenticationService/view-document.js
@@ -1,29 +1,28 @@
 //Function Initialization
 $(document).ready(function () {
-	document.getElementById("loadingPage").style.display = "block";
-	/* Check session */
-	loginData = sessionGet("loginAccount");
-	if (loginData == null) {
-		//redirect users to login page
-		window.location.href = "../login.html";
-	}
-	else {
-		//Get user control access range
-		displayDocument(FHIRURL, 'DocumentReference', '?author=' + loginData.userSelectedRole + "&_sort=-_lastUpdated", FHIRResponseType, 'listDocs');
-	}
+    document.getElementById("loadingPage").style.display = "block";
+    /* Check session */
+    loginData = sessionGet("loginAccount");
+    if (loginData == null) {
+        //redirect users to login page
+        window.location.href = "../login.html";
+    }
+    else {
+        //Display document
+        displayDocument(FHIRURL, 'DocumentReference', '?author=' + loginData.userSelectedRole + "&_sort=-_lastUpdated", FHIRResponseType, 'listDocs');
+    }
 });
 
 //Display document
-function displayDocument()
-{
+function displayDocument() {
     let obj = loginData.selectedDocRef;
 
-    document.getElementById("DocRefID").innerHTML="DocumentReference/" + obj.fullUrl.split('/').pop();
+    document.getElementById("DocRefID").innerHTML = "DocumentReference/" + obj.fullUrl.split('/').pop();
 
-    let date = (obj.resource.date)? obj.resource.date.replace("T", " ").substring(0, 16) : '';
-    let category = (obj.resource.category)? obj.resource.category[0].coding[0].display : '';
-    let subject = (obj.resource.subject.display)? obj.resource.subject.display : obj.resource.subject.reference;
-    let author =  (obj.resource.author[0].display)? obj.resource.author[0].display : obj.resource.author[0].reference;
+    let date = (obj.resource.date) ? obj.resource.date.replace("T", " ").substring(0, 16) : '';
+    let category = (obj.resource.category) ? obj.resource.category[0].coding[0].display : '';
+    let subject = (obj.resource.subject.display) ? obj.resource.subject.display : obj.resource.subject.reference;
+    let author = (obj.resource.author[0].display) ? obj.resource.author[0].display : obj.resource.author[0].reference;
     let endPoints = "";
     obj.resource.content.map((endpoint, i) => {
         endPoints += endpoint.attachment.title + ": " + endpoint.attachment.url + "<br>";
@@ -31,16 +30,99 @@ function displayDocument()
 
     let headers = ["Date", "Document Type", "Subject", "Author", "Endpoints"];
     let values = [date, category, subject, author, endPoints]
-    
-    for (i=0; i<headers.length; i++){
+
+    for (i = 0; i < headers.length; i++) {
         let tr = document.createElement("tr");
         let th = document.createElement("th");
-        th.innerHTML=headers[i];
+        th.innerHTML = headers[i];
         let td = document.createElement("td");
-        td.innerHTML=values[i];
+        td.innerHTML = values[i];
         tr.appendChild(th);
         tr.appendChild(td);
         document.getElementById("DocTable").appendChild(tr);
     }
+}
 
+function getToken() {
+    var str = selectedDocRef.split('<br>');
+    var JWTEndpoint = "";
+    str.map((endpoint, i) => {
+        JWTEndpoint += JWTToken(loginData, endpoint) + ",";
+        //endPoints += endpoint.attachment.title +" : "+endpoint.attachment.url +"<br>";
+    })
+    alert(JWTEndpoint);
+    //var param = JWTToken(loginData);//AESencryptString(str)
+    var decParam = AESdecryptString(params);
+    var urlParams = new URLSearchParams(decParam);
+    var useFor = urlParams.get('useFor')
+    var redirect_uri = urlParams.get('redirect_uri')
+    var param = AESencryptString(JWTEndpoint)
+    //document.cookie = "token=asY-x34SfYPk";
+    window.open(redirect_uri + '?token=' + encodeURI(param), "_self");
+
+    get(JWTAPI, 'application/x-www-form-urlencoded', formData, 'Role/index.html');
+    psotResource
+}
+
+function getAccessToken() {
+
+    //Using XMLHttpRequest component to interact with the server
+    var xhttp = new XMLHttpRequest();
+    /*
+        xhttp.open(method, url, async)
+        @desc： Initialize components
+        @param： 
+            method： Using HTTP "GET" method
+            url： Request path
+            async： synchronously(false) or asynchronously(true)
+    */
+    xhttp.open("POST", JWTAPIURL, false);
+    /*
+        xhttp.setRequestHeader(header, value)
+        @desc： Set the value of the HTTP header
+        @param：
+            header： Header name
+            value： Header value
+    */
+    xhttp.setRequestHeader("Content-type", 'text/' + FHIRResponseType);
+    /*
+        xhttp.onreadystatechange = callback;
+        @desc：Stores a function to be called automatically each time the readyState property changes
+    */
+    xhttp.onreadystatechange = function () {
+        /*
+            this.readyState
+            @desc： Return the current status of the XMLHttpRequest
+            @value：
+                0: request not initialized
+                1: server connection established
+                2: request received (can obtained header & status)
+                3: processing request
+                4: request finished and response is ready
+        */
+        /*
+            this.status
+            @desc：HTTP status messages that might be returned
+            @value：
+                0：UNSENT or OPENED
+                200：LOADING or DONE
+                403:FORBIDDEN
+                404:PAGE NOT FOUND
+        */
+        if (this.readyState == 4 && (this.status == 200 || this.status == 201)) {
+            var str = this.response;
+            loginData = JSON.parse(this.response);
+            sessionSet("loginAccount", loginData, 30);
+            window.open(redirect_uri, "_self");
+        }
+        else if (this.readyState == 4 && (this.status != 200 || this.status != 201)) {
+            alert(this.response);
+        }
+    };
+    /*
+        xhttp.send()
+        @desc： Send a request to the specified server path
+    */
+   
+    xhttp.send(JSON.stringify(loginData));
 }
--- LIVE/portal/portal-includes/js/setting.js
+++ LIVE/portal/portal-includes/js/setting.js
@@ -1,10 +1,11 @@
 //Set FHIR Server URL and response type (json or xml)
 
 let FHIRURLLogin= 'http://203.64.84.33:33484/api/Login';
-let FHIRURLRegister= 'https://tzfhir.ml:53443/process/register';
-let FHIRURL= 'https://203.64.84.150:58443/r5/fhir/'; //default FHIR Server API 
-let SelectCourseAPI= "https://tcumi.ml:443/api/LMSAPI/SelectCourse"; //Select course API
-let FHIRResponseType= 'json';					//Requested data type returned by the server
+let JWTAPIURL= 'http://203.64.84.33:33484/api/jwt';
+let FHIRURLRegister = 'https://tzfhir.ml:53443/process/register';
+let FHIRURL = 'https://203.64.84.150:58443/r5/fhir/'; //default FHIR Server API 
+let SelectCourseAPI = "https://tcumi.ml:443/api/LMSAPI/SelectCourse"; //Select course API
+let FHIRResponseType = 'json';					//Requested data type returned by the server
 
 // let FHIRURLLogin= 'https://tzfhir.ml:53443/rest/login';
 // let FHIRURLRegister= 'https://tzfhir.ml:53443/process/register';
@@ -13,7 +14,7 @@ let FHIRResponseType= 'json';					//Requested data type returned by the server
 // let FHIRResponseType= 'json';					//Requested data type returned by the server
 
 //Define required FHIR resources
-let DB={
-	organization: "854", 
+let DB = {
+	organization: "854",
 	schedule: "860"
 };
