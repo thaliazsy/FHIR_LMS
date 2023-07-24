338c7b5a578238840866f86684a93d8bcb52cf98
Thalia <thalia_noga@yahoo.com>
2023-07-24 17:01:16 +0800

    update server url

--- LIVE/portal/assets/js/0-global/setting.js
+++ LIVE/portal/assets/js/0-global/setting.js
@@ -3,8 +3,8 @@
 //let FHIRURLLogin= 'http://203.64.84.33:33484/api/login';
 //let JWTAPIURL= 'http://203.64.84.33:33484/api/jwt';
 
-let apiRoot = "https://localhost:44327/";	// Development
-//let apiRoot = "https://203.64.84.33:33484/";	// Release
+//let apiRoot = "https://localhost:44327/";	// Development
+let apiRoot = "https://tcumi.com:33484/";	// Release
 
 let FHIRURLRegister = apiRoot + 'api/register';
 let FHIRURLLogin = apiRoot+ 'api/login';
@@ -12,7 +12,7 @@ let JWTAPIURL = apiRoot + 'api/jwt';
 let MiniAppsURL = apiRoot + 'api/role/mini-apps';
 let SelectCourseAPI = apiRoot + "api/select-course"; //Select course API
 
-let FHIRURL = 'https://203.64.84.150:58443/r5/fhir/'; //default FHIR Server API
+let FHIRURL = 'https://fhir.tcumi.com:58443/r5/fhir/'; //default FHIR Server API
 let SkinLesionReportViewer = 'http://203.64.84.32:9876/viewer';
 
 let FHIRResponseType = 'json';					//Requested data type returned by the server
